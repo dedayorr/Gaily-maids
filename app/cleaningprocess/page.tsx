59dc53ac7b3a19f61e2280fa0dd2cab2911d6cb5
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2024-06-12 19:45:40 +0100

    process

--- app/cleaningprocess/page.tsx
+++ app/cleaningprocess/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import styles from "./cleaningprocess.module.css";
 
 const CleaningProcess = () => {
   return (
@@ -11,27 +12,32 @@ const CleaningProcess = () => {
       <h2 className="font-light text-[28px] lg:text-[35px] lg:tracking-widest">
         Full Service House Cleaning
       </h2>
-      <div>
-        <p className="leading-8 lg:leading-[200%] text-justify flex-1 lg:text-[28px]">
-          When you&apos;re balancing a busy family life, long hours at the office,
-          and other important aspects of your weekly routine, finding time to
-          keep your house in order can be difficult. Even though life gets busy,
-          coming home should never be a chore and with Gaily Maids, it won&quot;t
-          be. We understand your home is your space to unwind and feel safe and
-          secure. Our professional house cleaning services are designed to keep
-          your home looking its best, based on your cleaning preferences and
-          desired frequency. Learn about our room by room cleaning process and
-          how our professional house cleaning services can help restore health
-          and balance to your life.
+      <div
+        className={` lg:flex-row-reverse lg:gap-[10%] lg:mx-[10%] lg:flex lg:justify-between`}
+      >
+        <p className="mb-[5%] leading-8 lg:leading-[180%] text-justify lg:text-[22px] lg:w-[8]">
+          When you&apos;re balancing a busy family life, long hours at the
+          office, and other important aspects of your weekly routine, finding
+          time to keep your house in order can be difficult. Even though life
+          gets busy, coming home should never be a chore and with Gaily Maids,
+          it won&quot;t be. We understand your home is your space to unwind and
+          feel safe and secure. Our professional house cleaning services are
+          designed to keep your home looking its best, based on your cleaning
+          preferences and desired frequency. Learn about our room by room
+          cleaning process and how our professional house cleaning services can
+          help restore health and balance to your life.
         </p>
         <Image
-            className=" "
-            src="/dirt.png"
-            alt=""
-            width={200}
-            height={100}
-            layout="responsive"
-          />
+          className={`${styles.process}`}
+          src="/cleanprocess.png"
+          alt=""
+          width={100}
+          height={100}
+        />
+      </div>
+
+      <div>
+        
       </div>
     </div>
   );
