6e8983d024332fec8bb543a7e26f6b3ed1a81612
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2024-06-25 09:07:20 +0100

    24th-june

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -73,22 +73,25 @@ const Header = () => {
               <FaTimes className="text-3xl text-white" />
             )}
           </div>
-          <div className=" hidden md:flex gap-[10px] text-[14px] text-white lg:gap-[25px] lg:text-[20px] ">
+          <div className=" hidden md:flex gap-[10px] text-[14px] text-white lg:gap-[25px] lg:text-[18px] ">
             <Link href="/cleaning-services">
-              <p className="lg:hover:text-2xl">Cleaning Services</p>
+              <p className="lg:hover:text-xl">Cleaning Services</p>
             </Link>
 
             <Link href="/cleaning-process">
-              <p className="lg:hover:text-2xl">Our Cleaning Process</p>
+              <p className="lg:hover:text-xl">Our Cleaning Process</p>
             </Link>
             <Link href="/why-us">
-              <p className="lg:hover:text-2xl">Why Hire Us</p>
+              <p className="lg:hover:text-xl">Why Hire Us</p>
             </Link>
             <Link href="/aboutUs">
-              <p className="lg:hover:text-2xl">About Us</p>
+              <p className="lg:hover:text-xl">About Us</p>
             </Link>
             <Link href="/hiring">
-              <p className="lg:hover:text-2xl">We&apos;re Hiring</p>
+              <p className="lg:hover:text-xl">We&apos;re Hiring</p>
+            </Link>
+            <Link href="/faq">
+              <p className="lg:hover:text-xl">Faq</p>
             </Link>
           </div>
         </div>
@@ -105,7 +108,7 @@ const Header = () => {
                 onClick={closeNav}
               />
             </Link>
-            <Accordion className=" md:hidden border-none flex flex-col gap-[25px] mt-[10px] px-[5%]">
+            <Accordion className=" md:hidden border-none flex flex-col gap-[15px] mt-[10px] px-[5%]">
               <Accordion.Panel>
                 <Accordion.Title className="text-[#823ec9] text-[18px] border-none">
                   Cleaning Services
@@ -190,14 +193,21 @@ const Header = () => {
             >
               <Link href="/hiring">We&apos;re Hiring </Link>
             </p>
-           
-              <button
-                onClick={openModal}
-                className="ml-[5%] mt-[15%] text-[20px] italic text-[#5f2b96] underline"
-              >
-                Request an Estimate
-              </button>
-         
+
+            <p
+              onClick={closeNav}
+              className="ml-[5%] text-[18px] mt-[8%] font-medium text-[#823ec9]"
+            >
+              <Link href="/faq">Faq</Link>
+            </p>
+
+            <button
+              onClick={openModal}
+              className="ml-[5%] mt-[9%] text-[20px] italic text-[#5f2b96] underline"
+            >
+              Request an Estimate
+            </button>
+
             <div className="flex text-[#5f2b96] gap-[5%] items-center ml-[5%] mt-[5%] underline text-[20px] italic ">
               {/* <FaPhoneVolume /> */}
               <a href="tel:+18473939704">
