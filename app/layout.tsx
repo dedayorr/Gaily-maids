bd28808c20aa5f3bef89697921b546d135ec9b77
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2024-06-26 09:13:34 +0100

    logochange

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Header from "./components/header/header";
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
