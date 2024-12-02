97bab5a0d32c5e6ccf51191b8db0e686c54365d8
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2024-12-02 17:04:50 +0100

    offer

---
+++ app/offerPopUp/Offer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Modal from "../components/Modal/Modal";
+
+function Offer() {
+  return (
+    <>
+      <Modal>
+        <div>Offer</div>
+      </Modal>
+    </>
+  );
+}
+
+export default Offer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 // import Image from "next/image";
 // import Link from "next/link";
 // import Header from "./components/Header/Header";
@@ -8,19 +8,28 @@ import Process from "./components/contents/Process/Process";
 import Hero from "./components/contents/HeroSection/Hero";
 import EmbraceHealth from "./components/contents/EmbraceHealth/EmbraceHealth";
 import Faq from "./components/contents/FAQ/Faq";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="text-black ">
       <div className="lg:text-[20px]">
-       
-        <Hero/>
+        {/* <div className="bg-[#6b9ff9] h-[70px] w-[70px] fixed right-0 top-[50%] rounded-lg text-[20px] font-bold text-white">
+Click For Offer
+       </div> */}
+        <Image
+          className="fixed right-0 bottom-[5%]"
+          src="/specialOffer.png"
+          height={300}
+          width={130}
+          alt=""
+        />
+        <Hero />
         <Offers />
-        <Process/>
-        <EmbraceHealth/>
-        <Faq/>
+        <Process />
+        <EmbraceHealth />
+        <Faq />
         {/* <Link href="/users">about us</Link> */}
-        
       </div>
     </main>
   );
